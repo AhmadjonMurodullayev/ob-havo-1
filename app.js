935c0573a151f14a16745a4b84edc3829d490dc2
AhmadjonMurodullayev <murodullayevahmadjon26@gmail.com>
2024-10-14 18:29:47 +0500

    js

--- app.js
+++ app.js
@@ -1,47 +1,97 @@
-let id = '9505fd1df737e20152fbd78cdb289b6a';
-let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + id;
-let city = document.querySelector('.name');
-let form = document.querySelector("form");
-let temperature = document.querySelector('.temperature');
-let description = document.querySelector('.description');
-let valueSearch = document.getElementById('name');
-let clouds = document.getElementById('clouds');
-let humidity = document.getElementById('humidity');
-let pressure = document.getElementById('pressure');
-let main = document.querySelector('main');
+const id = '9505fd1df737e20152fbd78cdb289b6a'; 
+const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${id}`;
+const cityElement = document.querySelector('.name');
+const form = document.querySelector("form");
+const temperatureElement = document.querySelector('.temperature');
+const descriptionElement = document.querySelector('.description');
+const searchInput = document.getElementById('name');
+const cloudsElement = document.getElementById('clouds');
+const humidityElement = document.getElementById('humidity');
+const pressureElement = document.getElementById('pressure');
+const mainElement = document.querySelector('main');
+const forecastSection = document.querySelector('.forecast-grid');
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();  
-    if(valueSearch.value != ''){
-        searchWeather();
+    if (searchInput.value !== '') {  
+        fetchWeatherData();
     }
 });
-const searchWeather = () => {
-    fetch(url+'&q='+ valueSearch.value)
-        .then(response => response.json())
+
+const fetchWeatherData = () => {
+    fetch(`${url}&q=${searchInput.value}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('City not found');
+            }
+            return response.json();
+        })
         .then(data => {
             console.log(data);
-            if(data.cod == 200){
-                city.querySelector('figcaption').innerHTML = data.name;
-                city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
-                temperature.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
-                temperature.querySelector('span').innerText = data.main.temp;
-                description.innerText = data.weather[0].description;
-
-                clouds.innerText = data.clouds.all;
-                humidity.innerText = data.main.humidity;
-                pressure.innerText = data.main.pressure;
-            }else{
-                main.classList.add('error');
-                setTimeout(() => {
-                    main.classList.remove('error');
-                }, 1000);
-            }
-            valueSearch.value = '';
+            updateWeatherUI(data);
+            return fetchForecastData();
         })
-}
-// search Default
+        .then(forecastData => {
+            console.log(forecastData);
+            displayForecast(forecastData);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            mainElement.classList.add('error'); 
+            setTimeout(() => {
+                mainElement.classList.remove('error');  
+            }, 1000);
+        })
+        .finally(() => {
+            searchInput.value = ''; 
+        });
+};
+
+const updateWeatherUI = (data) => {
+    cityElement.querySelector('figcaption').innerText = data.name; 
+    cityElement.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
+    temperatureElement.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`; 
+    temperatureElement.querySelector('span').innerText = `${data.main.temp}°C`;
+    descriptionElement.innerText = data.weather[0].description;  
+
+    cloudsElement.innerText = `${data.clouds.all}%`;  
+    humidityElement.innerText = `${data.main.humidity}%`; 
+    pressureElement.innerText = `${data.main.pressure} hPa`;  
+};
+
+const fetchForecastData = () => {
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${id}&q=${searchInput.value}`)
+        .then(response => response.json());
+};
+
+const displayForecast = (forecastData) => {
+    forecastSection.innerHTML = '';
+    const dailyData = [];
+    
+    forecastData.list.forEach(item => {
+        const date = new Date(item.dt_txt);
+        if (date.getHours() === 12) { 
+            dailyData.push(item);
+        }
+    });
+
+    dailyData.forEach(day => {
+        const forecastEl = document.createElement('div');
+        forecastEl.classList.add('forecast-day');
+        const date = new Date(day.dt_txt).toLocaleDateString(undefined, { weekday: 'long', day: 'numeric', month: 'short' });
+        forecastEl.innerHTML = `
+            <h3>${date}</h3>
+            <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="Weather icon">
+            <p>${day.weather[0].description}</p>
+            <p><strong>${day.main.temp}°C</strong></p>
+        `;
+        forecastSection.appendChild(forecastEl);
+    });
+};
+
 const initApp = () => {
-    valueSearch.value = 'Washington';
-    searchWeather();
-}
+    searchInput.value = 'Tashkent';  
+    fetchWeatherData(); 
+};
+
 initApp();
